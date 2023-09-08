c71efae0ae53e5a7bbdddf2fb819adcc4b5428a3
anthony-unamma <anthonyunamma@gmail.com>
2023-09-08 17:33:55 +0100

    Add JS to display current day and UTC time in milliseconds

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-//Setting up current day of week and current time
+// create a new Date object
+const now = new Date();
+
+// get the current day of the week
+const dayOfWeek = now.getDay();
+
+// get the name of the day of the week
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const dayName = daysOfWeek[dayOfWeek];
+
+const dayDisplay = document.querySelector('p.day');
+const timeInMilliDisplay = document.querySelector('p.time');
+
+const utcTimeInMilliSeconds = Date.now();
+
+//Function to get new UTC time in milliseconds
+function liveTimeUpdate() {
+    const newUtcTimeInMilliSeconds = Date.now();
+
+    timeInMilliDisplay.innerText = newUtcTimeInMilliSeconds;
+}
+
+dayDisplay.innerText = dayName;
+timeInMilliDisplay.innerText = utcTimeInMilliSeconds;
+
+//Set interval to update the time on the page every 1sec
+setInterval(liveTimeUpdate, 1000);
